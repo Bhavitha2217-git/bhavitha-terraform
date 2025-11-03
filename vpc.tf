ae13ef3bd45b6d9448a72abe63ff66137ea0d87c
Bhavitha2217-git <BHAVITHAKOTESWAR137@GMAIL.COM>
2025-11-03 10:17:33 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -28,7 +28,7 @@ data "aws_security_group" "sg-default" {
 
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.selected.id]
+    values = [data.aws_vpc.vpc.id]
   }
 }
 
@@ -62,6 +62,7 @@ resource "aws_route_table_association" "rt-association2" {
   route_table_id = aws_route_table.rt2.id
   subnet_id      = aws_subnet.public-subnet2.id
 }
+
 
 
 
